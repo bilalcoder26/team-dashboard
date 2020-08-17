f0036878a555c0bf7a8fb5c6a0874b012bc638db
Bilal Ahmed <ahmedalibilal26@gmail.com>
2020-08-17 15:24:21 +0530

    form reset functinality added

--- src/component/header/create-team/CreateTeam.js
+++ src/component/header/create-team/CreateTeam.js
@@ -1,39 +1,57 @@
-import React from 'react'
+import React from "react";
 import { useDispatch } from "react-redux";
 
-import * as actionTypes from '../../../store/actions';
-
+import * as actionTypes from "../../../store/actions";
 
 function CreateTeam() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    dispatch({ type: actionTypes.ADDTEAM, val: evt.target.optName.value });
+    evt.target.reset();
+  };
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        dispatch({type:actionTypes.ADDTEAM, val:evt.target.optName.value});
-
-    }
-    
-    return (
-        <form className="create-team-form form" onSubmit={handleSubmit}>
-            <div className="form-row">
-                <div className="col-4">
-                    <label htmlFor="inlineFormInputName2">Select Type</label>
-                    <select className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe" required>
-                        <option value="" disabled hidden>Choose Any...</option>
-                        <option value="Team">Team</option>
-                    </select>
-                </div>
-                <div className="col-4">
-                    <label htmlFor="inlineFormInputGroupUsername2">Team Name</label>
-                    <input type="text" className="form-control" id="inlineFormInputGroupUsername2" name="optName" placeholder="Team Name" required/>
-                </div>
-                <div className="col-3">
-                    <button id="btn-create-team" type="submit" className="btn btn-warning mb-2 ">Create</button>
-                </div>
-            </div> 
-        </form>
-    )
+  return (
+    <form className="create-team-form form" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <div className="col-4">
+          <label htmlFor="inlineFormInputName2">Select Type</label>
+          <select
+            className="form-control mb-2 mr-sm-2"
+            id="inlineFormInputName2"
+            placeholder="Jane Doe"
+            required
+          >
+            <option value="" disabled hidden>
+              Choose Any...
+            </option>
+            <option value="Team">Team</option>
+          </select>
+        </div>
+        <div className="col-4">
+          <label htmlFor="inlineFormInputGroupUsername2">Team Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inlineFormInputGroupUsername2"
+            name="optName"
+            placeholder="Team Name"
+            required
+          />
+        </div>
+        <div className="col-3">
+          <button
+            id="btn-create-team"
+            type="submit"
+            className="btn btn-warning mb-2 "
+          >
+            Create
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default CreateTeam;
--- src/store/reducers/teams.js
+++ src/store/reducers/teams.js
@@ -1,51 +1,57 @@
-import * as actionTypes from '../actions';
-import _ from 'lodash';
+import * as actionTypes from "../actions";
+import _ from "lodash";
 
 const initialState = {
-    teams: [],
-    currTeam:{}
+  teams: [],
+  currTeam: {},
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
-        case actionTypes.ADDTEAM:
-            return {
-                ...state,
-                teams: state.teams.concat({teamName: action.val, usersList:[]})
-            }
-        case actionTypes.SELECTTEAM:
-            const teamObj = state.teams.find(obj=>(obj.teamName===state.currTeam.teamName));
-            return {
-                ...state,
-                currTeam: {teamName: action.val, usersList: teamObj?teamObj.usersList:[]}
-            }
-        case actionTypes.ADDUSER:
-            let users = _.cloneDeep(state.currTeam.usersList).concat(action.val);
-            let teams = _.cloneDeep(state.teams);
-            let team = {teamName: state.currTeam.teamName, usersList: users};
-            teams[teams.findIndex(obj=>(obj.teamName===state.currTeam.teamName))]=team;
-            return {
-                ...state,
-                currTeam: team,
-                teams: teams
-            }
-        case actionTypes.DELETEUSER:
-            let newUsers = _.cloneDeep(state.currTeam.usersList)
-            newUsers.splice(action.val,1);
-            teams = state.teams
-            team = {teamName: state.currTeam.teamName, usersList: newUsers};
-            alert(teams.findIndex(obj=>(obj.teamName===state.currTeam.teamName)));
-            teams[teams.findIndex(obj=>(obj.teamName===state.currTeam.teamName))]=team;
-            return {
-                ...state,
-                currTeam: team,
-                teams:teams
-                
-            }
-        default:
-            break;
-    }
-    return state;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADDTEAM:
+      return {
+        ...state,
+        teams: state.teams.concat({ teamName: action.val, usersList: [] }),
+      };
+    case actionTypes.SELECTTEAM:
+      const teamObj = state.teams.find((obj) => obj.teamName === action.val);
+      return {
+        ...state,
+        currTeam: {
+          teamName: action.val,
+          usersList: teamObj ? teamObj.usersList : [],
+        },
+      };
+    case actionTypes.ADDUSER:
+      let users = _.cloneDeep(state.currTeam.usersList).concat(action.val);
+      let teams = _.cloneDeep(state.teams);
+      let team = { teamName: state.currTeam.teamName, usersList: users };
+      teams[
+        teams.findIndex((obj) => obj.teamName === state.currTeam.teamName)
+      ] = team;
+      return {
+        ...state,
+        currTeam: team,
+        teams: teams,
+      };
+    case actionTypes.DELETEUSER:
+      let newUsers = _.cloneDeep(state.currTeam.usersList);
+      newUsers.splice(action.val, 1);
+      let teamDel = {
+        teamName: state.currTeam.teamName,
+        usersList: newUsers,
+      };
+      state.teams[
+        state.teams.findIndex((obj) => obj.teamName === state.currTeam.teamName)
+      ] = teamDel;
+      return {
+        ...state,
+        currTeam: teamDel,
+      };
+    default:
+      break;
+  }
+  return state;
 };
 
 export default reducer;
